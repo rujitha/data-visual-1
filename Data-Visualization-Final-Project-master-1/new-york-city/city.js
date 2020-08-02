188057da44f802ae0deb4f5a8a710a622811ac99
senthil ramachandran <senthil.ramachandran@refinitiv.com>
2020-08-02 08:49:06 -0400

    removed

--- Data-Visualization-Final-Project-master-1/new-york-city/city.js
+++
@@ -1,5 +0,0 @@
-dataDate = new Date(2016, 9, 1); //October 01, 2016
-loadNeighbourhoods("../data/neighbourhoods.csv");
-loadListings("../data/listings.csv");
-//loadNeighbourhoodBoundaries("../data/neighbourhoods.geojson");
-//reviewsFile = "../data/reviews.csv";
